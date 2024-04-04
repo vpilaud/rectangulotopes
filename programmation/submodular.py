7cd1a6bdf4f2e538610768bc6c2c39c437b4eb6e
Jean <jcardin@jeans-mbp.info.ulb.ac.be>
2024-04-04 15:00:12 +0200

    improved submodular.py

--- programmation/submodular.py
+++ programmation/submodular.py
@@ -4,40 +4,49 @@ from itertools import *
 
 ## SUBMODULAR FUNCTIONS
 
-# simplex for a Loday arc I
-# 1 if S and I intersects, 0 otherwise
+# simplex for an up arc I
+# 1 if S intersects I, 0 otherwise
 def f_loday(S, I):
     return int(bool(set(S) & set (I)))
 
-# anti-simplex for an anti-Loday arc I
+# anti-simplex for a down arc I
 # -1 if I is a subset of S, 0 otherwise
 def f_antiloday(S, I):
     return - int(set(I).issubset(set(S)))
 
 # simplex + antisimplex (cuboctahedron in 3d)
-# 1 if S and I intersects but I is not a subset of S, 0 otherwise
+# 1 if S intersects I but I is not a subset of S, 0 otherwise
 def f_cuboc(S, I):
     return int(bool(set(S) & set (I)) and not set(I).issubset(set(S)))
 
-# weak rectangulation
-# number of I such that S and I intersects but I is not a subset of S
+# weak rectangulotopes
+# number of I such that S intersects I but I is not a subset of S
 def f_weakrect(S, n):
     return sum([int(bool(set(S) & set (range(i, j+1))) and not set(range(i, j+1)).issubset(set(S))) for i in range(n-1) for j in range(i+1, n)])
 
 # yin shard polytopes
-# 1 if and only if S intersects I but J is not a subset of S
+# 1 if and only if S intersects I, and J is not a subset of S
 def f_yin(S, I, J):
     return int(bool(set(S) & set(I)) and not (set(J).issubset(S)))
 
-# yin rectangulations
-# number of pairs I, J such that S intersects I but J is not a subset of S
+# yin rectangulotopes
+# number of pairs I, J such that S intersects I, and J is not a subset of S
 def f_yinrect(S, n):
     return sum([int (bool(set(S) & set(range(i, j+1))) and not (set(range(j+1, k+1)).issubset(S))) for i in range(n-1) for j in range(i, n-1) for k in range(j+1, n)])
 
 # yang shard polytopes
-# 1 if and only if S intersects J but I is not a subset of S
+# 1 if and only if S intersects J, and I is not a subset of S
 def f_yang(S, I, J):
     return int(bool(set(S) & set(J)) and not (set(I).issubset(S)))
+
+# yang rectangulotopes
+# number of pairs I, J such that S intersects J, and I is not a subset of S
+def f_yangrect(S, n):
+    return sum([int (bool(set(S) & set(range(j+1, k+1))) and not (set(range(i, j+1)).issubset(S))) for i in range(n-1) for j in range(i, n-1) for k in range(j+1, n)])
+
+# strong rectangulotopes
+def f_strongrect(S, n):
+    return f_yinrect(S, n) + f_yangrect(S, n)
 
 ## GENERIC FUNCTIONS
 
@@ -49,55 +58,40 @@ def powerset(iterable):
 
 # compute the polyhedron for a given submod function
 def submod_polytope(f, n):
-    all_ineq = []
-    for S in powerset(range(n)):
-        all_ineq.append((f(S),) + tuple(-1 if x in S else 0 for x in range(n)))
-    return Polyhedron(Polyhedron(ieqs=all_ineq).vertices()) # remove rays
+    all_ineq = [ (f(S),) + tuple(-1 if x in S else 0 for x in range(n)) for S in powerset(range(n)) ]
+    the_eq = [ (f(range(n)),) + tuple(-1 for i in range(n)) ]
+    return Polyhedron(ieqs = all_ineq, eqns = the_eq)
 
 ## RECTANGULOTOPES
 
 # associahedron as a Minkowski sum
 def assoc(n):
-    A = Polyhedron([(0,)*n])
-    for i in range(n-1):
-        for j in range(i+1, n):
-            I = range(i, j+1)
-            g_loday = lambda S : f_loday(S, I)
-            A = A + submod_polytope(g_loday, n)
-    return Polyhedron(A.vertices()) # remove rays
+    return sum([submod_polytope(lambda S : f_loday(S, range(i, j+1)), n) for i in range(n-1) for j in range(i+1, n)])
 
 # weak rectangulation polytope (diagonal / Baxter)
 # either sum all lodays and antilodays, or the cubocs
 def wrp(n):
-    A = Polyhedron([(0,)*n])
-    for i in range(n-1):
-        for j in range(i+1, n):
-            I = range(i, j+1)
-            # g_loday = lambda S : f_loday(S, range(i, j+1))
-            # g_antiloday = lambda S : f_antiloday(S, range(i, j+1))
-            # A = A + submod_polytope(g_loday, n) + submod_polytope(g_antiloday, n)
-            g = lambda S : f_cuboc(S, I)
-            A = A + submod_polytope(g, n)
-    return Polyhedron(A.vertices()) # remove rays
+    return sum([submod_polytope(lambda S : f_cuboc(S, I), n) for i in range(n-1) for j in range(i+1, n)])
 
 # weak rectangulotope via the summed submodular function f_weakrect 
 def wrp2(n):
-    return Polyhedron(submod_polytope(lambda S : f_weakrect(S, n), n).vertices())
+    return submod_polytope(lambda S : f_weakrect(S, n), n)
 
 # yin rectangulotopes via yin shards
 def yrp(n):
-    A = sum([submod_polytope(lambda S : f_yin(S, range(i, j+1), range(j+1, k+1)), n) for i in range(n-1) for j in range(i, n-1) for k in range(j+1, n)])
-    return Polyhedron(A.vertices()) # remove rays
+    return sum([submod_polytope(lambda S : f_yin(S, range(i, j+1), range(j+1, k+1)), n) for i in range(n-1) for j in range(i, n-1) for k in range(j+1, n)])
 
 # yin rectangulotopes via the summed submod function
 def yrp2(n):
-    return Polyhedron(submod_polytope(lambda S : f_yinrect(S, n), n).vertices())
+    return submod_polytope(lambda S : f_yinrect(S, n), n)
 
 # yang polytopes, just the same
 def yarp(n):
-    A = sum([submod_polytope(lambda S : f_yang(S, range(i, j+1), range(j+1, k+1)), n) for i in range(n-1) for j in range(i, n-1) for k in range(j+1, n)])
-    return Polyhedron(A.vertices()) # remove rays
+    return sum([submod_polytope(lambda S : f_yang(S, range(i, j+1), range(j+1, k+1)), n) for i in range(n-1) for j in range(i, n-1) for k in range(j+1, n)])
 
+# strong rectangulotope
+def srp(n):
+    return submod_polytope(lambda S: f_strongrect(S, n), n)
 
 # examples
 #WRP6 = wrp2(6)
